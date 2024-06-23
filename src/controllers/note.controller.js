92ff0077a593cd36288e1c0ebec3ab2f88533713
sabin6969 <sabinpoudel6969@gmail.com>
2024-06-23 13:40:06 +0530

    feat: Add CRUD operations for notes

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -11,7 +11,7 @@ const createNote = asyncHandler(async(req,res)=>{
         .json(new ApiResponse(400,"All fields are required",false))
     }
     else{
-        // req.user is provided by authmiddleware
+        // req.user is provided by authmiddleware by decoding access token
         const owner = await User.findById(req.user._id);
         const note = await Note.create(
             {
@@ -26,7 +26,72 @@ const createNote = asyncHandler(async(req,res)=>{
     }
 });
 
+const deleteNote = asyncHandler(async(req,res)=>{
+    const noteId = req?.params?.noteId;
+    if (noteId.match(/^[0-9a-fA-F]{24}$/)) {
+        // Yes, it's a valid ObjectId, proceed with `findById` call.
+        // The noteId provided by us is in the format of string but findById requires it to be in its own format like an object 
+        // Stackoverflow reference
+        // https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id#:~:text=It%20can%20be%20corrected%20by%20making%20path%20of%20both%20routes%20different.&text=I%20went%20with%20an%20adaptation,casting%20as%20a%20validation%20method.&text=Always%20use%20mongoose.,Types.
+        
+        res.status(400).json(new ApiResponse(400,"Note id is invalid"))
+    }
+    else{
+        const note = await Note.findById(noteId);
+    if(!note){
+        res.status(404).json(new ApiResponse(404,"Requested note doesnot exist",false));
+    }
+    else{
+        if(note.owner.toString()===req.user._id.toString()){
+            await Note.findByIdAndDelete(noteId);
+            res.status(200).json(new ApiResponse(200,"Note deleted",true))
+        }
+        else{
+            res.status(403).json(new ApiResponse(403,"Forbidden to perform this operation"))
+        }
+    }
+    }
+});
+
+const updateNote = asyncHandler(async(req,res)=>{
+    const noteId = req?.params?.noteId;
+    if (!noteId.match(/^[0-9a-fA-F]{24}$/)) {
+        // Yes, it's a valid ObjectId, proceed with `findById` call.
+        // StackOverflow
+        // https://stackoverflow.com/questions/14940660/whats-mongoose-error-cast-to-objectid-failed-for-value-xxx-at-path-id#:~:text=It%20can%20be%20corrected%20by%20making%20path%20of%20both%20routes%20different.&text=I%20went%20with%20an%20adaptation,casting%20as%20a%20validation%20method.&text=Always%20use%20mongoose.,Types.
+        res.status(400).json(new ApiResponse(400,"Note id is invalid",false));
+    }
+    else{
+        const note = await Note.findById(noteId);
+        if(!note){
+            res.status(404).json(new ApiResponse(404,"Note doesnot exits"));
+        }
+        else{
+            if(note.owner.toString()===req.user._id.toString()){
+                const {noteTitle,noteDescription} = req.body;
+                if([noteTitle,noteDescription].some(e=> typeof e ==="undefined") || [noteId,noteDescription].some(e=> e?.trim()==="")){
+                res.status(400).json(new ApiResponse(400,"All fields are required",false));
+                }
+                else{
+                const updatedNote = await Note.findByIdAndUpdate(noteId,{
+                    $set:{
+                        noteTitle,
+                        noteDescription,
+                    }
+                },{new:true});
+                res.status(200).json(new ApiResponse(200,"Note updated sucessfully",true,updatedNote))
+                }
+            }
+            else{
+                res.status(403).json(new ApiResponse(403,"Forbidden to perform this operation"))
+            }
+        }
+        
+    }
+});
 
 export {
     createNote,
+    deleteNote,
+    updateNote,
 }
--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { createNote } from "../controllers/note.controller.js";
+import { createNote, deleteNote, updateNote } from "../controllers/note.controller.js";
 
 const noteRouter = express.Router();
 
 noteRouter.route("/createNote").post(authMiddleware,createNote);
-
+noteRouter.route("/deleteNote/:noteId").delete(authMiddleware,deleteNote);
+noteRouter.route("/updateNote/:noteId").patch(authMiddleware,updateNote);
 export default noteRouter;
